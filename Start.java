3f2b92215af35f828a93b458857430202b867ccf
AldanTai <142383968+AldanTai@users.noreply.github.com>
2024-01-12 16:51:56 +0800

    Add files via upload

---
+++ Start.java
@@ -0,0 +1,16 @@
+import LBNgames.*;
+
+
+public class Start {
+
+	public static void main(String[] args) {
+		
+		Start start = new Start();
+		
+		Interface_control IC= new Interface_control();
+
+		IC.setVisible(true);
+		
+	}
+
+}
